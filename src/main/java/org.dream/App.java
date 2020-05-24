4eee3a5173b9b2dfd89171a27b95f6e9845b57a1
aorxsr <9624344i>
2020-05-24 22:31:57 +0800

    测试启动

---
+++ src/main/java/org.dream/App.java
@@ -0,0 +1,15 @@
+package org.dream;
+
+import org.ael.Ael;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Ael ael = new Ael();
+
+        ael.start();
+
+    }
+
+}
