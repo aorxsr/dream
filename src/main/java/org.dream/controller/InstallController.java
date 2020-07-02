3be0629ab0a6acc56bbde549fbe1e5018ba95ca1
aorxsr <9624344i>
2020-07-02 19:14:31 +0800

    修改一些没用的东西

--- src/main/java/org.dream/controller/InstallController.java
+++ src/main/java/org.dream/controller/InstallController.java
@@ -2,7 +2,6 @@ package org.dream.controller;
 
 import org.ael.c.annotation.Controller;
 import org.ael.c.annotation.GetMapping;
-import org.ael.c.annotation.RequestMapping;
 
 @Controller
 public class InstallController {
